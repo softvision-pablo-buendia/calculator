2991281fc775a841882f6a1865ecb9b068ae8535
pablo.buendia <pablo.buendia@softvision.com>
2021-09-14 11:02:30 -0300

    add mysql and h2 support

--- src/main/java/com/pablo/calculator/domain/Customer.java
+++ src/main/java/com/pablo/calculator/domain/Customer.java
@@ -2,7 +2,10 @@ package com.pablo.calculator.domain;
 
 import lombok.*;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +17,10 @@ import java.util.UUID;
 @AllArgsConstructor
 public class Customer {
 
+    @Id
+    @GeneratedValue
     private UUID id;
     private String name;
+    @ElementCollection(targetClass=String.class)
     private List<String> results;
 }
